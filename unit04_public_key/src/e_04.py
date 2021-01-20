7b12382285094f7f752fe8bf2308527a8a514ed1
Bill Buchanan <w.buchanan@napier.ac.uk>
2021-01-20 19:04:47 +0000

    Update e_04.py

--- unit04_public_key/src/e_04.py
+++ unit04_public_key/src/e_04.py
@@ -1,5 +1,6 @@
 import rsa
 (bob_pub, bob_priv) = rsa.newkeys(512)
-ciphertext = rsa.encrypt('Here is my message', bob_pub)
+ciphertext = rsa.encrypt('Here is my message'.encode(), bob_pub)
 message = rsa.decrypt(ciphertext, bob_priv)
 print(message.decode('utf8'))
+
