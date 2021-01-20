35ed84c7b3bb09b76ea071a7616b69bbd5049e45
Bill Buchanan <w.buchanan@napier.ac.uk>
2021-01-20 19:08:34 +0000

    Update e_01.py

--- unit04_public_key/src/e_01.py
+++ unit04_public_key/src/e_01.py
@@ -5,10 +5,11 @@ PHI=(p-1)*(q-1)
 e=3
 for d in range(1,100):
 	if ((e*d % PHI)==1): break
-print e,N
-print d,N
+print (e,N)
+print (d,N)
 M=4
 cipher = M**e % N
-print cipher
+print (cipher)
 message = cipher**d % N
-print message
+print (message)
+
