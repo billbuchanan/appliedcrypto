e29eacacf24e1782ef7aa9a53a328f1b6b2a55e6
Bill Buchanan <w.buchanan@napier.ac.uk>
2021-01-20 19:30:16 +0000

    Update d_01.py

--- unit04_public_key/src/d_01.py
+++ unit04_public_key/src/d_01.py
@@ -7,24 +7,24 @@ test="Test123"
 alice = pyelliptic.ECC() 
 bob = pyelliptic.ECC()
 
-print "++++Keys++++"
-print "Bob's private key: "+bob.get_privkey().encode('hex')
-print "Bob's public key: "+bob.get_pubkey().encode('hex')
+print ("++++Keys++++")
+print ("Bob's private key: "+bob.get_privkey().encode('hex'))
+print ("Bob's public key: "+bob.get_pubkey().encode('hex'))
 
-print
-print "Alice's private key: "+alice.get_privkey().encode('hex')
-print "Alice's public key: "+alice.get_pubkey().encode('hex')
+print()
+print ("Alice's private key: "+alice.get_privkey().encode('hex'))
+print ("Alice's public key: "+alice.get_pubkey().encode('hex'))
 
 
 ciphertext = alice.encrypt(test, bob.get_pubkey())
 
-print "\n++++Encryption++++"
+print ("\n++++Encryption++++")
 
-print "Cipher: "+ciphertext.encode('hex')
+print ("Cipher: "+ciphertext.encode('hex'))
 
-print "Decrypt: "+bob.decrypt(ciphertext)
+print ("Decrypt: "+bob.decrypt(ciphertext))
 
 signature = bob.sign("Alice")
 
-print 
-print "Bob verified: "+ str(pyelliptic.ECC(pubkey=bob.get_pubkey()).verify(signature, "Alice"))
+print ()
+print ("Bob verified: "+ str(pyelliptic.ECC(pubkey=bob.get_pubkey()).verify(signature, "Alice")))
