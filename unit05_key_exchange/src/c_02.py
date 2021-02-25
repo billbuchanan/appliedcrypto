14ce2ed5c69ee5ce4885a56b331ae8d684a9dba0
Bill Buchanan <w.buchanan@napier.ac.uk>
2021-02-25 19:14:32 +0000

    Update c_02.py

--- unit05_key_exchange/src/c_02.py
+++ unit05_key_exchange/src/c_02.py
@@ -1,22 +1,19 @@
 import sys
-import random
 
 p=11
 
 def getG(p):
 
   for x in range (1,p):
-	rand = x
-	exp=1
-	next = rand % p
+	  rand = x
+	  exp=1
+	  next = rand % p
 
-	while (next <> 1 ):
-		next = (next*rand) % p
-		exp = exp+1
-		
+	  while (next != 1 ):
+		  next = (next*rand) % p
+		  exp = exp+1
+	  if (exp==p-1):
+	    print (rand)
 
-	if (exp==p-1):
-		print rand
-
-print getG(p)
+print (getG(p))
 
